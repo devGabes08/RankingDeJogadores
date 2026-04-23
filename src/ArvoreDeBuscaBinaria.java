3e5f1a9ac4f8a2086a3660003dd37a8bcb6bd45c
devGabes08 <dev.gabriel08@gmail.com>
2026-04-23 08:56:03 -0300

    Foi criado o Método de insercao()

--- src/ArvoreDeBuscaBinaria.java
+++ src/ArvoreDeBuscaBinaria.java
@@ -1,2 +1,24 @@
 public class ArvoreDeBuscaBinaria {
+    private Node raiz;
+
+    public ArvoreDeBuscaBinaria() {
+        this.raiz = null;
+    }
+
+    public void insercao(Player player) {
+        raiz = insercao(raiz, player);
+    }
+
+    private Node insercao(Node atual, Player player) {
+        if (atual == null) {
+            return new Node(player);
+        }
+
+        if (player.getRanking() < atual.getPlayer().getRanking()) {
+            atual.setEsquerda(insercao(atual.getEsquerda(), player));
+        } else if (player.getRanking() > atual.getPlayer().getRanking()) {
+            atual.setDireita(insercao(atual.getDireita(), player));
+        }
+        return atual;
+    }
 }
